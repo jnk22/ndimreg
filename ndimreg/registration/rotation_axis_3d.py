abb33f4fcb544d306a71a6b664071c2c4c49f86a
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2025-02-22 15:58:51 +0100

    chore: reduce code

--- ndimreg/registration/rotation_axis_3d.py
+++ ndimreg/registration/rotation_axis_3d.py
@@ -46,7 +46,7 @@ if TYPE_CHECKING:
 
 SRC: Final = (0, 1, 2)
 DEST: Final = {0: (0, 1, 2), 1: (1, 2, 0), 2: (2, 0, 1)}
-FLIPS: Final = (..., (*(slice(None),) * 2, slice(None, None, -1)))
+FLIPS: Final = (..., (..., slice(None, None, -1)))
 
 
 class RotationAxis3DRegistration(BaseRegistration):
